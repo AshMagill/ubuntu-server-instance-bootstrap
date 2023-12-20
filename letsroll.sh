dc2455acc1dfa3d44e85aba9e03d440642702076
ashmagill <ashmagill@developersinstitute.co.nz>
2023-12-20 19:17:15 +1300

    fixed small bug in docker isntall

--- letsroll.sh
+++ letsroll.sh
@@ -1,46 +1,54 @@
 #!/bin/bash
-# Prompt for GitHub repo link
-read -p "Enter the GitHub repo link: " repo_link
-# Check if Docker is already installed
+# Check if docker-compose is installed
+if ! command -v docker-compose &> /dev/null; then
+    echo "docker-compose is not installed. Installing..."
+    # Install docker-compose
+    sudo apt-get update
+    sudo apt-get install -y docker-compose
+fi
+# Check if docker is installed
 if ! command -v docker &> /dev/null; then
-    echo "Docker not found. Installing Docker..."
+    echo "docker is not installed. Installing..."
+    # Install docker
     sudo apt-get update
     sudo apt-get install -y docker.io
-else
-    echo "Docker is already installed."
+    sudo systemctl start docker
+    sudo systemctl enable docker
 fi
-# Check if Docker Compose is already installed
-if ! command -v docker-compose &> /dev/null; then
-    echo "Docker Compose not found. Installing Docker Compose..."
-    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-else
-    echo "Docker Compose is already installed."
-fi
-# Clone the GitHub repo
-git clone $repo_link
-repo_name=$(basename $repo_link .git)
-cd $repo_name
+# Prompt for GitHub repo link
+read -p "Enter the GitHub repo link: " repo_link
 # Check if the repo is private
-repo_info=$(curl -s -u $username:$github_key "https://api.github.com/repos/$repo_name")
-is_private=$(echo $repo_info | jq -r '.private')
-if [[ $is_private == "true" ]]; then
-    # Prompt for GitHub credentials
-    read -p "Enter your GitHub username: " username
-    read -p "Enter your GitHub key: " github_key
-    # Configure GitHub credentials for cloning private repos
-    git config --global credential.helper store
-    git config --global user.name $username
-    git config --global user.password $github_key
+if [[ $repo_link == *"github.com"* ]]; then
+    if [[ $repo_link == *"https://"* ]]; then
+        repo_link=${repo_link/https:\/\//}
+    elif [[ $repo_link == *"http://"* ]]; then
+        repo_link=${repo_link/http:\/\//}
+    fi
+    repo_info=$(curl -s "https://api.github.com/repos/$repo_link")
+    is_private=$(echo "$repo_info" | grep -o '"private":.*' | awk '{print $2}')
+    if [[ $is_private == "true," ]]; then
+        # Prompt for username and private key
+        read -p "Enter your GitHub username: " username
+        read -s -p "Enter your private key: " private_key
+        echo
+        # Download the private repo using curl with authentication
+        curl -u "$username:$private_key" -L "$repo_link" -o repo.zip
+    else
+        # Download the public repo using curl
+        curl -L "$repo_link" -o repo.zip
+    fi
+else
+    echo "Invalid GitHub repo link."
+    exit 1
 fi
-# Pull the latest changes from the repo
-git pull
-# Run any additional commands or setup steps here
-# Reset GitHub credentials
-git config --global --unset user.name
-git config --global --unset user.password
-# Change permissions for docker-compose
-sudo chmod +x docker-compose
-# Start Docker Compose
-sudo docker-compose up -d
+# Unzip the downloaded repo
+unzip repo.zip
+# Clean up the downloaded zip file
+rm repo.zip
+# Change directory to the downloaded repo
+cd repo
+# Run any necessary commands on the downloaded repo
+# ...
+# Finish the script
+echo "Script completed successfully."
 
