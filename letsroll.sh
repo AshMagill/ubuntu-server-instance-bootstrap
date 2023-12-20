410f7c46fa5808fa4587bfde296faf8256e90c28
ashmagill <ashmagill@developersinstitute.co.nz>
2023-12-20 16:08:43 +1300

    removed sudo requirement

--- letsroll.sh
+++ letsroll.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-# Run as sudo
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
 # Prompt for GitHub username and password
 read -p "Enter your GitHub username: " username
 read -s -p "Enter your GitHub password: " password
 echo
+# Prompt for the repository link
+read -p "Enter the full GitHub repository link: " repo_link
+# Prompt for the repository password
+read -s -p "Enter your GitHub repository password: " repo_password
+# Check if running as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 # Install Docker
 apt-get update
 apt-get install -y docker.io
@@ -17,14 +21,9 @@ chmod +x /usr/local/bin/docker-compose
 # Add current user to docker group
 usermod -aG docker $USER
 # Clone the GitHub repository
-read -p "Enter the full GitHub repository link: " repo_link
 git clone $repo_link
-# Prompt for the repository password
-read -s -p "Enter your GitHub repository password: " repo_password
-echo
 # Authenticate with GitHub
 echo $repo_password | docker login https://docker.pkg.github.com -u $username --password-stdin
 # Build and run the Docker Compose
 cd Ecommerce-Business-Onepager
 docker-compose up -d
-
